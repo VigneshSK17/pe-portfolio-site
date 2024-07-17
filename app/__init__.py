b4cddbd2a01542b5e493895d033f701f10ba5309
Heet Shah <58372785+HeetShah@users.noreply.github.com>
2024-07-17 15:14:43 -0400

    Add tests to DB, content, and API (#22)

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,15 @@ from playhouse.shortcuts import model_to_dict
 load_dotenv()
 app = Flask(__name__)
 
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file::memory:?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
               user=os.getenv("MYSQL_USER"),
               password=os.getenv("MYSQL_PASSWORD"),
               host=os.getenv("MYSQL_HOST"),
               port=3306)
-
-print(mydb)
 
 class TimelinePost(Model):
     name = CharField()
@@ -155,12 +157,22 @@ def get_time_line_post():
 
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
-    timeline_post = TimelinePost.create(name=name, email=email, content=content)
+    name = request.form.get('name')
+    email = request.form.get('email')
+    content = request.form.get('content')
+    
+    if not name:
+        return {'error': 'Invalid name'}, 400
+    if not content:
+        return {'error': 'Invalid content'}, 400
+    if '@' not in email or '.' not in email:
+        return {'error': 'Invalid email'}, 400
+    try:
+        timeline_post = TimelinePost.create(name=name, email=email, content=content)
+        return model_to_dict(timeline_post), 201
+    except Exception as e:
+        return {'error': str(e)}, 500
 
-    return model_to_dict(timeline_post)
 
 @app.route('/api/timeline_post/latest', methods=['DELETE'])
 def delete_latest_time_line_post():
