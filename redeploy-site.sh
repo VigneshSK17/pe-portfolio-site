bdffb9fa4a2977d6a816767ba92e470874dd2f55
VigneshSK17 <vigsk17@gmail.com>
2024-07-26 23:03:33 +0000

    Tweaked deployment script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,4 +6,4 @@ git fetch && git reset origin/main --hard
 
 docker compose -f docker-compose.prod.yml down
 
-docker compose -f docker-compose.prod.yml up -d --build
+docker compose -f docker-compose.prod.yml up -d
